e0f3de9e8b8559e393fd340724c21348a1e1952e
Eric McDonald <eric.d.mcdonald@gmail.com>
2021-05-21 22:49:32 -0400

    Fix the database connection being dumb.

--- back-end-server/config/database.js
+++ back-end-server/config/database.js
@@ -3,25 +3,8 @@ const bcrypt = require("bcryptjs");
 const accountModel = require("../models/account");
 const appConstants = require("../config/appConstants");
 
-module.exports.dbConnection = mySql.createConnection(appConstants.mySqlCfg);
+module.exports.connectionPool = mySql.createPool(appConstants.mySqlCfg);
 
-function connectToDb() {
-    module.exports.dbConnection.connect(function(err) {
-        if (err) {
-            console.log("Failed to connect to the MySQL database with error " + err + ".");
-        }
-    });
-}
 module.exports.init = function() {
-    module.exports.dbConnection.on("error", (err) => {
-        // Eric: re-establishes the connection to the database because for some reason mysql decides to just drop it after a while...
-        module.exports.dbConnection = mySql.createConnection(appConstants.mySqlCfg);
-        accountModel.init(module.exports.dbConnection);
-        connectToDb();
-    });
-    connectToDb();
-    accountModel.init(module.exports.dbConnection);
-};
-module.exports.cleanup = function() {
-    module.exports.dbConnection.close();
+    accountModel.init(module.exports.connectionPool);
 };
--- back-end-server/models/account.js
+++ back-end-server/models/account.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcryptjs");
 const objUtil = require("../objectUtil");
 
-var database;
+var dbConnectionPool;
 
 module.exports.registerAccount = function(account) {
     const accountObj = account;
@@ -13,26 +13,28 @@ module.exports.registerAccount = function(account) {
                 reject("Malformed request. Trying to hack the server?");
                 return;
         }
-        database.query("SELECT * FROM Account WHERE email='" + accountObj.email + "'", function (error, results, fields) {
-            if (error) {
-                reject("An error occurred while creating the account: " + error);
-                return;
-            }
-            if (results.length != 0) {
-                reject("Account already exists.");
-            } else {
-                var salt = bcrypt.genSaltSync(10);
-                var hash = bcrypt.hashSync("B4c0/\/", salt);
-                accountObj.password = hash;
-                console.log("INSERT INTO Account (email, password_hash, permissions_lvl, language) VALUES('" + accountObj.email + "', '" + accountObj.password + "', " + 
-                accountObj.permissionsLvl + ", '" + accountObj.language + "')");
-                database.query("INSERT INTO Account (email, password_hash, permissions_lvl, language) VALUES('" + accountObj.email + "', '" + accountObj.password + "', " + 
-                accountObj.permissionsLvl + ", '" + accountObj.language + "')");
-                resolve("Created your account successfully.");
-            }
+        dbConnectionPool.getConnection((err, connection) => {
+            connection.query("SELECT * FROM Account WHERE email='" + accountObj.email + "'", function (error, results, fields) {
+                if (error) {
+                    reject("An error occurred while creating the account: " + error);
+                    return;
+                }
+                if (results.length != 0) {
+                    reject("Account already exists.");
+                } else {
+                    var salt = bcrypt.genSaltSync(10);
+                    var hash = bcrypt.hashSync("B4c0/\/", salt);
+                    accountObj.password = hash;
+                    console.log("INSERT INTO Account (email, password_hash, permissions_lvl, language) VALUES('" + accountObj.email + "', '" + accountObj.password + "', " + 
+                    accountObj.permissionsLvl + ", '" + accountObj.language + "')");
+                    connection.query("INSERT INTO Account (email, password_hash, permissions_lvl, language) VALUES('" + accountObj.email + "', '" + accountObj.password + "', " + 
+                    accountObj.permissionsLvl + ", '" + accountObj.language + "')");
+                    resolve("Created your account successfully.");
+                }
+            });
         });
     });
 }
-module.exports.init = function(dbConnection) {
-    database = dbConnection;
+module.exports.init = function(connectionPool) {
+    dbConnectionPool = connectionPool;
 }
