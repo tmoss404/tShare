5d2ec80ca40d3abad93ca626f9945f41decf6c81
tmoss404 <tmoss404@gmail.com>
2021-06-01 15:23:04 -0400

    npm update audit fix

--- front-end-server/src/app/register/register.component.ts
+++ front-end-server/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../User';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private accountService: AccountService) { }
+
+  userAccount: User = new User;
 
   ngOnInit(): void {
+  }
+
+  createAccount(){
+    console.log("Submit Submit!");
   }
 
 }
