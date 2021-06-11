fcba9c3a0a5d869b45d4e98b364d30b76bd9b600
tmoss404 <tmoss404@gmail.com>
2021-06-11 03:00:57 -0400

    Update on navigation, simple logout function

--- front-end-server/src/app/header/header.component.ts
+++ front-end-server/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationEnd } from '@angular/router';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router, private auth : AuthenticationService) { }
 
-  ngOnInit(): void {
+  public token: any;
+  
+  ngOnInit() {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationStart || event instanceof NavigationEnd) { // only read the token on "NavigationStart"
+        this.token = this.auth.readToken();
+      }
+    });
   }
 
+  logout() {
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("expires_at");
+  }
+  
 }
