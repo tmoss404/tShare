d8954457fe0a278c419609c25806eaee3fd06367
tmoss404 <tmoss404@gmail.com>
2021-06-11 01:15:19 -0400

    Login component complete

--- front-end-server/src/app/login/login.component.ts
+++ front-end-server/src/app/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../User';
+import { AuthenticationService } from '../authentication.service';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm : FormGroup;
 
-  ngOnInit(): void {
+  response : {
+    message: string,
+    success: boolean
   }
+
+  constructor( 
+    private auth : AuthenticationService,
+    private router : Router, 
+    private formBuilder : FormBuilder
+  ) { }
+
+  ngOnInit() : void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  login() : void {
+
+    const user = this.loginForm.value;
+    
+    this.auth.login(user).subscribe(
+      (success) => {
+        const expiresAt = moment().add(success.expiresIn,'second');
+
+        localStorage.setItem( 'access_token',success.loginToken );
+        localStorage.setItem( "expires_at", JSON.stringify(expiresAt.valueOf()) );
+        this.response = success;
+      },
+      (err) => {
+        this.response = err.error;
+      }
+    );
+
+  }
+
+  get form() { return this.loginForm.controls }
 
 }
