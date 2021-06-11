825ab2a254621e5d9b81dd9fb057be1b1a0360be
tmoss404 <tmoss404@gmail.com>
2021-06-10 21:43:57 -0400

    added JWT authentication service

---
+++ front-end-server/src/app/authentication.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+const helper = new JwtHelperService();
+
+import { User } from './User';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthenticationService {
+
+  constructor( private http: HttpClient) { }
+
+  public getToken(): string {
+    return localStorage.getItem('access_token');
+  }
+
+  public readToken(): any{
+    const token = localStorage.getItem('access_token');
+    return helper.decodeToken(token);
+  }
+
+  isAuthenticated(): boolean {
+    const token = localStorage.getItem('access_token');
+
+    if(token) {
+      if(helper.isTokenExpired(token))
+        return false;
+
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  login(user: User): Observable<any> {
+    return this.http.post<any>('http://tshare-back-end.herokuapp.com/account/login', user);
+  }
+}
