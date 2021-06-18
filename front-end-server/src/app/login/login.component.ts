9f8db01ac3d0440eef824147a92039a8c4cd63f6
tmoss404 <tmoss404@gmail.com>
2021-06-17 22:43:49 -0400

    Changed post login navigation, removed expiresAt property

--- front-end-server/src/app/login/login.component.ts
+++ front-end-server/src/app/login/login.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import * as moment from 'moment';
 
 @Component({
   selector: 'app-login',
@@ -37,11 +36,9 @@ export class LoginComponent implements OnInit {
     
     this.auth.login(user).subscribe({
       next: (success) => {
-        const expiresAt = moment().add(success.expiresIn,'second');
 
         localStorage.setItem( 'access_token', success.loginToken );
-        localStorage.setItem( "expires_at", JSON.stringify(expiresAt.valueOf()) );
-        this.router.navigate(['/my-files']);
+        this.router.navigate(['/file-dashboard']);
       },
       error: (err) => {
         this.response = err.error;
