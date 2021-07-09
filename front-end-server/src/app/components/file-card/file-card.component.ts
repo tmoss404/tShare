dc0ec234bc9716dd1deef75e5f222b4945f3d702
tmoss404 <tmoss404@gmail.com>
2021-07-09 10:21:46 -0400

    File card update

--- front-end-server/src/app/components/file-card/file-card.component.ts
+++ front-end-server/src/app/components/file-card/file-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AccountService } from 'src/app/services/account.service';
 
 @Component({
   selector: 'app-file-card',
@@ -8,16 +9,26 @@ import { Component, Input, OnInit } from '@angular/core';
 export class FileCardComponent implements OnInit {
 
   @Input() file: any;
+  selected: boolean | false;
+  dateFormat: string | 'M/d/yy, h:mm a';
 
-  constructor() { }
+  constructor(
+    private accountService: AccountService
+  ) { }
 
   ngOnInit(): void {
     if(this.file)
       this.file.name = this.getLastItem();
+
+    this.dateFormat = this.accountService.getDateFormat();
   }
 
   getLastItem() : string {
     const fileRoute = this.file.Key;
     return this.file.Key.substring(fileRoute.lastIndexOf('/') + 1);
+  }
+
+  fileCardSelect(){
+    this.selected = !this.selected;
   }
 }
