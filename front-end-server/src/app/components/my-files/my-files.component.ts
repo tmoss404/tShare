13631c4b9b936fcde3f014ca68908d905f49df22
tmoss404 <tmoss404@gmail.com>
2021-07-14 13:23:07 -0400

    Context menu implementation, folder open on double click, file type icon, new folder

--- front-end-server/src/app/components/my-files/my-files.component.ts
+++ front-end-server/src/app/components/my-files/my-files.component.ts
@@ -11,6 +11,8 @@ export class MyFilesComponent implements OnInit, OnDestroy {
   files: Array<any>;
   fileToUpload: File;
   filesSub: any;
+  currentDir: string = null;
+  testDirName: string = "new folder";
 
   constructor(
     private fileService: FileService
@@ -25,10 +27,12 @@ export class MyFilesComponent implements OnInit, OnDestroy {
       this.filesSub.unsubscribe();
   }
 
-  getFiles(){
-    this.filesSub = this.fileService.getFiles().subscribe({
+  getFiles() {
+    this.filesSub = this.fileService.getFiles(this.currentDir).subscribe({
       next: (success) => {
         this.files = success.data.Contents;
+        console.log(this.files);
+        console.log('Current directory: ' + this.currentDir);
       },
       error: (err) => {
         console.log(err.error);
@@ -36,11 +40,11 @@ export class MyFilesComponent implements OnInit, OnDestroy {
     });
   }
 
-  uploadFile(files: FileList){
+  uploadFile(files: FileList) {
     if(files.item(0).name != null){
       this.fileToUpload = files.item(0);
 
-      let signUrlReq = this.fileService.getSignedUrl(this.fileToUpload);
+      let signUrlReq = this.fileService.getSignedUrl(this.fileToUpload, this.currentDir);
       if (signUrlReq != null) {
         signUrlReq.subscribe({
           next: (success) => {
@@ -59,6 +63,29 @@ export class MyFilesComponent implements OnInit, OnDestroy {
         });
       }
     }
+  }
+
+  createFolder() {
+    let newDir = this.currentDir != null ? this.currentDir + '/' + this.testDirName : this.testDirName;
+    this.fileService.createDir(newDir).subscribe({
+      next: (success) => {
+        this.getFiles();
+      },
+      error: (err) => {
+        console.log(err.error);
+      }
+    });
+  }
+
+  changeCurrentDir(dirName: string) {
+    if(this.currentDir == null) {
+      this.currentDir = dirName;
+    }
+    else {
+      this.currentDir = this.currentDir + '/' + dirName; 
+    }
+
+    this.getFiles();
   }
 
 }
