0f85a21a6f1dc7776491fe6a0d4db318e699b818
Mikhail <mishafrolov882000@gmail.com>
2021-07-16 19:05:46 +0600

    utility/log - first try

--- back-end-server/models/utility.js
+++ back-end-server/models/utility.js
@@ -1,5 +1,6 @@
 const objectUtil = require("../objectUtil");
 const commonErrors = require("./commonErrors");
+const appConstants = require("../config/appConstants");
 
 module.exports.logError = function(logErrData) {
     return new Promise((resolve, reject) => {
@@ -13,5 +14,71 @@ module.exports.logError = function(logErrData) {
         - You can check out the Trustifi documentation here for more information on sending an email using Trustifi: https://api.trustifi.com/#ad7d9d33-b431-2bb0-14e9-01c967eae2be 
         - Lastly, please note that error/success messages must contain the following properties: message, httpStatus, success. Please see how I'm sending this information in one of the models in the models folder, such as in account.js.
         */
+
+        // Declaring incoming values
+        var recipient = logErrData.recipient;
+        var receivedMessage = logErrData.message;
+
+        // Validating the logErrData
+        if (objectUtil.isNullOrUndefined(logErrData) || objectUtil.isNullOrUndefined(receivedMessage) || objectUtil.isNullOrUndefined(recipient) || receivedMessage.length == 0 || recipient.length == 0) {
+            reject(commonErrors.genericStatus400);
+
+            return;
+        }
+        // Received Error consol.logging
+        console.log("Received error: " + receivedMessage);
+
+
+        // Trustify
+        var trustifiOpts = {
+            'method': 'POST',
+            'url': 'https://be.trustifi.com/api/i/v1/email',
+            'headers': {
+                'x-trustifi-key': appConstants.trustifiKey,
+                'x-trustifi-secret': appConstants.trustifiSecret,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "recipients": [{
+                    "email": recipient
+                }],
+                "lists": [],
+                "contacts": [],
+                "attachments": [],
+                "title": "Error that occurred in the tShare application",
+                "html": "Hi,<br/><br/><br/>" +
+                    " This is an error: " + receivedMessage + ".<br/><br/>" +
+                    "Best,<br/><br/><br/>Mikhail Frolov - tShare",
+                "methods": {
+                    "postmark": false,
+                    "secureSend": false,
+                    "encryptContent": false,
+                    "secureReply": false
+                }
+            })
+        };
+        // Trustify
+        request(trustifiOpts, function(error, response) {
+            if (error) {
+                reject({
+                    message: "An error has occurred while sending the email.",
+                    httpStatus: 400,
+                    success: false
+                });
+                return;
+            } else {
+                resolve({
+                    message: receivedMessage,
+                    httpStatus: 200,
+                    success: true
+                });
+            }
+        });
+    }).catch((resultsNull) => {
+        reject({
+            message: "An error has occurred while sending the email.",
+            httpStatus: 500,
+            success: false
+        });
     });
 };
