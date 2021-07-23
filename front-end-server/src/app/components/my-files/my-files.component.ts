a74972b97602a3e541afc3c1c97b54fa517a2407
tmoss404 <tmoss404@gmail.com>
2021-07-23 06:29:05 -0400

    Added some comments for info on what is going on in my functions

--- front-end-server/src/app/components/my-files/my-files.component.ts
+++ front-end-server/src/app/components/my-files/my-files.component.ts
@@ -28,7 +28,8 @@ export class MyFilesComponent implements OnInit {
     this.getFiles(this.currentDir);
   }
 
-  async getFiles(dir: string) {
+  //Async getfiles function, returning a promise so I can execute certain operations after it resolves
+  async getFiles(dir: string) : Promise<any> {
     this.files = undefined;
 
     const files$ = this.fileService.getFiles(dir);
@@ -39,86 +40,16 @@ export class MyFilesComponent implements OnInit {
     });
   }
 
-  uploadFile(files: FileList) {
-    if(files.item(0).name != null){
-      this.fileToUpload = files.item(0);
-
-      let signUrlReq = this.fileService.getSignedUrlUpload(this.fileToUpload, this.currentDir);
-      if (signUrlReq != null) {
-        signUrlReq.subscribe({
-          next: (success) => {
-            this.fileService.uploadFile(success.signedUrlData, this.fileToUpload).subscribe({
-              next: (success) => {
-                this.getFiles(this.currentDir);
-              },
-              error: (err) => {
-                console.log(err.error);
-              }
-            });
-          },
-          error: (err) => {
-            console.log(err.error);
-          }
-        });
-      }
-    }
-  }
-
-  openNewFolderModal(content) {
-    this.newFolderForm = this.formBuilder.group({
-      folderName: ['', [Validators.required, folderName()]]
-    });
-
-    this.modalService.open(content, {centered: true, windowClass: 'new-folder-modal', size: 'sm'}); 
-  }
-
-  createFolder(modal: any) {
-    let folderName = this.newFolderForm.value.folderName;
-    let newDir = this.currentDir != (null || undefined) ? this.currentDir + '/' + folderName : folderName;
-
-    this.fileService.createDir(newDir).subscribe({
-      next: (success) => {
-        modal.close();
-        this.getFiles(this.currentDir);
-      },
-      error: (err) => {
-        console.log(err.error);
-      }
-    });
-    
-  }
-
-  changeCurrentDir(dirName: string) {
-    let newDir = this.currentDir == (null || undefined) ? dirName : this.currentDir + '/' + dirName;
-
-    this.getFiles(newDir).then(() => {
-      this.currentDir = newDir;
-    });
-  }
-
-  goBack() {
-    let newDir: string;
-
-    if(this.currentDir != (null || undefined)){
-      if(!this.currentDir.includes('/')) {
-        newDir = null;
-      }
-      else {
-        newDir = this.currentDir.substring(0, this.currentDir.lastIndexOf('/'));
-      }
-      this.getFiles(newDir).then(() => {
-        this.currentDir = newDir;
-      });
-    }
-  }
-
-  downloadFile(file: any) {
+  downloadFile(file: any) : void {
+    //We cannot download directories, so we disable the functionality for a file that is a directory
     if(!file.isDirectory){
       let filePath: string;
 
+      //Add a slash to the route if you are not in the root directory
       if(this.currentDir != (null || undefined)){
         filePath = this.currentDir + '/' + file.name;
       }
+      //File path is simply the name of the file if you are currently in the root directory
       else {
         filePath = file.name;
       }
@@ -148,4 +79,98 @@ export class MyFilesComponent implements OnInit {
       });
     }
   }
+
+
+  uploadFile(files: FileList) : void {
+    //Contingency for weird error with file input, trying to read null file name
+    if(files.item(0).name != null){
+      this.fileToUpload = files.item(0);
+
+      let signUrlReq = this.fileService.getSignedUrlUpload(this.fileToUpload, this.currentDir);
+      if (signUrlReq != null) {
+        signUrlReq.subscribe({
+          next: (success) => {
+            this.fileService.uploadFile(success.signedUrlData, this.fileToUpload).subscribe({
+              next: (success) => {
+                this.getFiles(this.currentDir);
+              },
+              error: (err) => {
+                console.log(err.error);
+              }
+            });
+          },
+          error: (err) => {
+            console.log(err.error);
+          }
+        });
+      }
+    }
+  }
+
+  //Opens the modal for creating a new folder
+  openNewFolderModal(content) : void {
+    //Creates a formgroup for entering the folder name
+    this.newFolderForm = this.formBuilder.group({
+      folderName: ['', [Validators.required, folderName()]]
+    });
+
+    this.modalService.open(content, {centered: true, windowClass: 'new-folder-modal', size: 'sm'}); 
+  }
+
+  //Creates a new folder in the system on submit of the modal form, takes the modal as an arg to close it on submit
+  createFolder(modal: any) {
+    //Get the user input from the form
+    let folderName = this.newFolderForm.value.folderName;
+    //If the current dir is the root then the new dir is simply the name of the directory, if not, add a slash in between
+    let newDir = this.currentDir != (null || undefined) ? this.currentDir + '/' + folderName : folderName;
+
+    //Post request to the back-end to create the folder
+    this.fileService.createDir(newDir).subscribe({
+      next: (success) => {
+        //Close the modal on success and update the files array
+        modal.close();
+        this.getFiles(this.currentDir);
+      },
+      error: (err) => {
+        console.log(err.error);
+      }
+    });
+    
+  }
+
+  //Changes current directory to the specified directory
+  changeCurrentDir(dirName: string) : void {
+    //If the current dir is the root then the new dir is simply the name of the directory, if not, add a slash in between
+    let newDir = this.currentDir == (null || undefined) ? dirName : this.currentDir + '/' + dirName;
+
+    //Update files array
+    this.getFiles(newDir).then(() => {
+      //Only change the current directory once the request completes and the files are updated
+      this.currentDir = newDir;
+    });
+  }
+
+  //Function to go back in the directory structure
+  goBack() : void {
+    //Placeholder for the new directory
+    let newDir: string;
+
+    //If the current directory is not the root dir
+    if(this.currentDir != (null || undefined)){
+      //If the current directory is the first directory past the root, redirect to the root
+      if(!this.currentDir.includes('/')) {
+        newDir = null;
+      }
+      //Remove the last directory from the route to navigate one backwards
+      else {
+        newDir = this.currentDir.substring(0, this.currentDir.lastIndexOf('/'));
+      }
+      //Update the files array
+      this.getFiles(newDir).then(() => {
+        //Only change the current directory once the request completes and the files are updated
+        this.currentDir = newDir;
+      });
+    }
+  }
+
 }
